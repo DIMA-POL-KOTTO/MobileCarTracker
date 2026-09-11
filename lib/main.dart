cb9538da047fa482e2be2c837c5b34cd2173f130
Dima Polubeiko <dpolubejko@gmail.com>
2026-09-11 18:55:32 +0300

    add button on fuelPage. Two btns to choice how to add check

--- lib/main.dart
+++ lib/main.dart
@@ -173,10 +173,26 @@ class FuelPage extends StatelessWidget {
           fontWeight: FontWeight.bold
         )
       ),
-      body: const Center(
-        child: Text('Страница заправок'),
-      ),
-    );
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+      ),    
+    ),
+    floatingActionButton: FloatingActionButton(
+      backgroundColor: Colors.blue,
+      foregroundColor: Colors.white,
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const AddFuelPage(),
+          ),
+        );
+      },
+      child: const Icon(Icons.add, size: 31,),
+    ),
+  );
   }
 }
 
@@ -221,6 +237,108 @@ class MorePage extends StatelessWidget {
       ),
       body: const Center(
         child: Text('Страница Ещё'),
+      ),
+    );
+  }
+}
+
+class AddFuelPage extends StatelessWidget {
+  const AddFuelPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        
+        title: const Text('Добавить заправку'),
+        titleTextStyle: const TextStyle(
+          color: Color.fromARGB(255, 0, 0, 0),
+          fontSize: 28,
+          fontWeight: FontWeight.bold,
+        )
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                height: 90,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Потом откроем камеру
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    
+                  ),
+                  child: const Row(
+                    children: [
+                      Icon(Icons.camera_alt, size: 32),
+                      SizedBox(width: 20),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Сфотографировать чек',
+                            style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold,
+                          ),
+                          ),
+                          SizedBox(height: 4),
+                          Text("Данные будут распознаны автоматически",
+                            style: TextStyle(fontSize: 13,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 15,),
+            SizedBox(
+              width: double.infinity,
+              height: 90,
+              child: ElevatedButton(
+                onPressed: () {}, 
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(16),
+                  ),
+                ),
+                child: const Row(
+                  children: [
+                    Icon(Icons.edit, size: 32),
+                    SizedBox(width: 20),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Ввести вручную',
+                          style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold,
+                        ),
+                        ),
+                        SizedBox(height: 4),
+                        Text("Самостоятельно указать данные заправки",
+                          style: TextStyle(fontSize: 13,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ],
+        ),
       ),
     );
   }
